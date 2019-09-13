c2f89d739379c9322280964bddb8ccd8a9695196
việt hồ đức <ducvietsqt@gmail.com>
2019-09-13 21:12:43 +0700

    add background led

--- src/components/static/StaticLedScreen.vue
+++ src/components/static/StaticLedScreen.vue
@@ -10,49 +10,596 @@
 
     <!--<img src="../../assets/led_screen_box.png" alt=""/>-->
     <!--<img src="../../assets/led_screen_eff.png" alt=""/>-->
-    <div class="list_lq">
-      <div class="list_lq-row">
-        <p class="item text_lq">
-          Liên Quân 1
-        </p>
-        <p class="item text_lq">
-          Liên Quân 2
-        </p>
-        <p class="item text_lq">
-          Liên Quân 3
-        </p>
-      </div>
-      <div class="list_lq-row">
-        <div class="item">
-          <div class="box_item">
-            Nguyen van a
-          </div>
-        </div>
-        <div class="item">
-          <div class="box_item">
-            Nguyen van a
-          </div>
-        </div>
-        <div class="item">
-          <div class="box_item">
-            Nguyen van a
-          </div>
-        </div>
-      </div>
-      <div class="list_lq-row"></div>
-      <div class="list_lq-row"></div>
-      <div class="list_lq-row"></div>
-      <div class="list_lq-row"></div>
-      <div class="list_lq-row"></div>
-      <div class="list_lq-row"></div>
-      <div class="list_lq-row"></div>
-      <div class="list_lq-row"></div>
-      <div class="list_lq-row"></div>
+    <div class="title_ping_game">
+      <img src="../../assets/led_title.png" alt=""/>
     </div>
+    <table class="list_lq">
+      <tr>
+        <td>
+          <p class="text_lq">
+            Liên Quân 1
+          </p>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+
+      </tr>
+      <tr>
+        <td>
+          <p class="text_lq">
+            Liên Quân 2
+          </p>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+
+      </tr>
+      <tr>
+        <td>
+          <p class="text_lq">
+            Liên Quân 3
+          </p>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+
+      </tr>
+      <tr>
+        <td>
+          <p class="text_lq">
+            Liên Quân 4
+          </p>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+
+      </tr>
+      <tr>
+        <td>
+          <p class="text_lq">
+            Liên Quân 5
+          </p>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+
+      </tr>
+      <tr>
+        <td>
+          <p class="text_lq">
+            Liên Quân 6
+          </p>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+
+      </tr>
+      <tr>
+        <td>
+          <p class="text_lq">
+            Liên Quân 7
+          </p>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+
+      </tr>
+      <tr>
+        <td>
+          <p class="text_lq">
+            Liên Quân 8
+          </p>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+
+      </tr>
+      <tr>
+        <td>
+          <p class="text_lq">
+            Liên Quân 9
+          </p>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+
+      </tr>
+      <tr>
+        <td>
+          <p class="text_lq">
+            Liên Quân 10
+          </p>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN 1
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN B
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+        <td>
+          <div class="box_item">
+            N.VĂN C
+          </div>
+        </td>
+
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+  import fs from '../../assets/led_title.png';
   export default {
     name: "StaticLedScreen"
   }
@@ -64,8 +611,10 @@
     overflow: hidden;
     width: 100vw;
     height: 100vh;
-    background-image: url("../../assets/led_screen_bg.jpg");
-    background-position: center;
+    overflow-y: auto;
+    /*background-image: url("../../assets/led_screen_bg.jpg");*/
+    background-image: url("../../assets/bg_no_title.jpg");
+    background-position: top center;
     background-size: 100%;
     background-repeat: no-repeat;
     color: #ffffff;
@@ -79,33 +628,45 @@
       display: block;
       margin: 0 auto;
     }
+    .title_ping_game {
+
+    }
     .list_lq {
-      display: flex;
       width: 87.5%;
-      margin: 31vh auto 0px auto;
-      .list_lq-row {
-        border: solid 1px blue;
-        width: calc(100%/11);
-        .item {
-          padding: 10px;
-          border: 1px solid;
+      margin: 0 auto 0px auto;
+      tr {
+        td {
+          padding-bottom: 60px;
         }
       }
+      td {
+        padding: 5px;
+      }
       .text_lq {
-        font-size: 30px;
+        font-size: 2.03125vw;
+        white-space: nowrap;
+        margin-right: 20px;
       }
       .box_item {
-        /*background: url("../../assets/led_screen_box.png");*/
-        /*background-size: cover;*/
         padding: 10px 20px;
         color: #000;
         background: #ffffff;
-        box-shadow:  inset 0px 1px 10px 4px rgba(0,0,0,0.2)
-        /*width: 236px;*/
-        /*height: 69px;*/
+        /*box-shadow: inset 0px 1px 10px 4px rgba(0, 0, 0, 0.2);*/
+        border-radius: 5px;
+        font-size: 1.6vw;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
 
       }
     }
-
+    .title_ping_game {
+      /*border: solid 1px blue;*/
+      margin: 5vh auto 0 auto;
+      img {
+        display: block;
+        margin: 0 auto;
+      }
+    }
   }
 </style>
